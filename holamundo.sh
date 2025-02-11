63bd3b50c1fb3293398924c712194f94c5be6005
lfergom <ferrergomez3@gmail.com>
2025-02-11 14:27:08 +0100

    Primer video meme

--- holamundo.sh
+++ holamundo.sh
@@ -4,6 +4,13 @@ echo "¡ATENCIÓN! 🚨🚨🚨"
 sleep 3
 echo "¡Tu sistema ha sido infectado por el virus de chill de cojones! 🤖💻"
 sleep 3
-$Meme_URL="https://www.youtube.com/watch?v=mNCz5mgWAsQ"
-xdg-open "$Meme_URL"
+
+# Primer video meme
+Meme_URL_1="https://www.youtube.com/watch?v=mNCz5mgWAsQ"
+echo "¡Activando meme de la abuela! 💣"
+sleep 2
+start "$Meme_URL_1"  # Abre el primer video meme en el navegador
+sleep 2  # Esperamos 2 segundos para dar un toque de suspenso
+echo "¡Pim pam toma lacasitos! 🎉"  # Frase graciosa
+sleep 2
 
