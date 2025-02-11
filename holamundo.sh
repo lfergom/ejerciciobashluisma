0381359a7608c90f63f7b521211cf6043da828ee
lfergom <ferrergomez3@gmail.com>
2025-02-11 14:09:43 +0100

    Meme

--- holamundo.sh
+++ holamundo.sh
@@ -4,3 +4,6 @@ echo "¡ATENCIÓN! 🚨🚨🚨"
 sleep 3
 echo "¡Tu sistema ha sido infectado por el virus de chill de cojones! 🤖💻"
 sleep 3
+$Meme_URL="https://www.youtube.com/watch?v=mNCz5mgWAsQ"
+xdg-open "$Meme_URL"
+
